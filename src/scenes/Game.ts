7e10dbdb6b6a632583b0f93454ef8564a217ca5d
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-03-28 10:57:45 +0100

    feat: update game dimensions and add initial assets for the game scene

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -7,7 +7,18 @@ export class Game extends Scene {
 
 	preload() {
 		this.load.setPath('assets');
+
+		this.load.image('sky', 'sky.png');
+		this.load.image('ground', 'platform.png');
+		this.load.image('star', 'star.png');
+		this.load.image('bomb', 'bomb.png');
+		this.load.spritesheet('dude', 'dude.png', {
+			frameWidth: 32,
+			frameHeight: 48,
+		});
 	}
 
-	create() {}
+	create() {
+		this.add.image(0, 0, 'sky').setOrigin(0, 0);
+	}
 }
