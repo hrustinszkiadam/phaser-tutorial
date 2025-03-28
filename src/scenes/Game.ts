294088ceac3f0888e77586458bc3d8f7fa708c59
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-03-28 11:19:45 +0100

    feat: add player configuration and adjust platform positions in the game scene

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,6 +2,7 @@ import { Scene } from 'phaser';
 
 export class Game extends Scene {
 	platforms: Phaser.Physics.Arcade.StaticGroup;
+	player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
 	constructor() {
 		super('Game');
@@ -21,13 +22,21 @@ export class Game extends Scene {
 	}
 
 	create() {
+		//world config
 		this.add.image(0, 0, 'sky').setOrigin(0, 0);
 
+		//platforms
 		this.platforms = this.physics.add.staticGroup();
 		this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-		this.platforms.create(600, 400, 'ground');
-		this.platforms.create(50, 250, 'ground');
+		this.platforms.create(600, 420, 'ground');
+		this.platforms.create(50, 260, 'ground');
 		this.platforms.create(750, 220, 'ground');
+
+		//player config
+		this.player = this.physics.add.sprite(100, 450, 'dude');
+		this.player.setBounce(0.2);
+		this.player.setCollideWorldBounds(true);
+		this.physics.add.collider(this.player, this.platforms);
 	}
 }
